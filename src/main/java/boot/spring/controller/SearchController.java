30419a716193fd232e57ab605c38148c9815eec0
mvymklkkl <mvymklkkl@outlook.com>
2021-10-07 04:10:19 +0800

    bugfix

--- src/main/java/boot/spring/controller/SearchController.java
+++ src/main/java/boot/spring/controller/SearchController.java
@@ -1,6 +1,5 @@
 package boot.spring.controller;
 
-import java.util.List;
 
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.sort.SortBuilders;
@@ -11,17 +10,17 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import boot.spring.elasticindex.ActorIndex;
 import boot.spring.elasticindex.SougoulogIndex;
-import boot.spring.pagemodel.ActorGrid;
-import boot.spring.po.Actor;
+import boot.spring.pagemodel.SougoulogSearchRequest;
 import boot.spring.repository.ActorRepository;
 import boot.spring.repository.SougoulogRepository;
+import boot.spring.service.SougoulogSearchService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
@@ -34,6 +33,43 @@ public class SearchController {
 	
 	@Autowired
 	SougoulogRepository sougoulogRepository;
+	
+	@Autowired
+	SougoulogSearchService sougoulogSearchService;
+	
+	@ApiOperation("多字段查询")
+	@RequestMapping(value="/multi_match",method = RequestMethod.POST)
+	@ResponseBody
+	public Page<SougoulogIndex> multi_match(@RequestBody SougoulogSearchRequest request) {
+		// 如果关键词为空，则返回所有
+		String content = request.getQuery().getKeyWords();
+		Integer rows = request.getQuery().getRows();
+		if (rows == null || rows == 0) {
+			rows = 10;
+		}
+		Integer start = request.getQuery().getStart();
+		int pagenum = start / rows;
+		// 第几页，页面大小
+		Pageable pageable = PageRequest.of(pagenum, rows);
+		if (content == null || "".equals(content)) {
+			return sougoulogSearchService.searchDefault(request, pageable);
+		} else {
+			return sougoulogSearchService.searchResult(request, pageable);
+		}
+	}	
+
+	@ApiOperation("query_string全字段查找")
+	@RequestMapping(value="/query_string",method = RequestMethod.POST)
+	@ResponseBody
+	public Page<SougoulogIndex> query_string(@RequestBody SougoulogSearchRequest request){
+		 // 构建查询条件
+	    NativeSearchQueryBuilder queryBuilder = new NativeSearchQueryBuilder();
+	    // 添加搜索条件
+	    queryBuilder.withQuery(QueryBuilders.queryStringQuery(request.getQuery().getKeyWords()));
+	    // 搜索，获取结果
+	    Page<SougoulogIndex> result = sougoulogRepository.search(queryBuilder.build());
+		return result;
+	}
 	
 	@ApiOperation("普通查询,先分词再精准搜索")
 	@RequestMapping(value="/matchSearch",method = RequestMethod.GET)
@@ -51,13 +87,15 @@ public class SearchController {
 	}
 	
 	@ApiOperation("精准查询,不分词直接搜索")
-	@RequestMapping(value="/termSearch",method = RequestMethod.GET)
+	@RequestMapping(value="/termSearch",method = RequestMethod.POST)
 	@ResponseBody
-	public Page<SougoulogIndex> termSearch(){
+	public Page<SougoulogIndex> termSearch(@RequestBody SougoulogSearchRequest request){
 		 // 构建查询条件
 	    NativeSearchQueryBuilder queryBuilder = new NativeSearchQueryBuilder();
+	    String field = request.getQueryFilter().get("field");
+	    String content = request.getQueryFilter().get("content");
 	    // 添加精准搜索条件
-	    queryBuilder.withQuery(QueryBuilders.termQuery("visittime", "00:00:00"));
+	    queryBuilder.withQuery(QueryBuilders.termQuery(field, content));
 	    // 搜索，获取结果
 	    Page<SougoulogIndex> result = sougoulogRepository.search(queryBuilder.build());
 		return result;
@@ -70,7 +108,7 @@ public class SearchController {
 		 // 构建查询条件
 	    NativeSearchQueryBuilder queryBuilder = new NativeSearchQueryBuilder();
 	    // 添加模糊搜索条件
-	    queryBuilder.withQuery(QueryBuilders.fuzzyQuery("visittime", "0:00:1"));
+	    queryBuilder.withQuery(QueryBuilders.fuzzyQuery("userid", "007177259245828"));
 	    // 搜索，获取结果
 	    Page<SougoulogIndex> result = sougoulogRepository.search(queryBuilder.build());
 		return result;
@@ -133,5 +171,6 @@ public class SearchController {
 	    // 搜索，获取结果
 	    Page<ActorIndex> result = actorRepository.search(queryBuilder.build());
 		return result;
-	}		
+	}
+	
 }
